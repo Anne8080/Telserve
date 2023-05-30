4d8e39bc2889be5581811c396ec34365c0c25c8a
Aje Damilola <ajedamilola2005@gmail.com>
2023-05-30 14:20:47 +0100

    Small Changes

--- src/pages/successful/Successful.jsx
+++ src/pages/successful/Successful.jsx
@@ -10,7 +10,7 @@ const Successful = () => {
             <div className="innersuccess">
               {/* <h2>Subscription Successful!</h2> */}
               <h1>Thank You For Subscribing With Us!</h1>
-              <h4>You will Be contacted in less than 3 Hours by our sales team</h4>
+              <h4>You will Be contacted soon by our sales team</h4>
               <div className="alink"><Link to="/" className="bluebtn a">Go To Home <EastOutlinedIcon id='sucicon' /></Link></div>
             </div>
         </div>
